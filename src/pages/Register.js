31ebd0081ad184fca2ad11a9a0bfce18d96919a0
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-08-29 21:53:43 +0300

    add register setup

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,51 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Logo } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const InitialState = {
+  name: "",
+  password: "",
+  email: "",
+  isMemder: true,
+};
 
 const Register = () => {
-  return <h1>Register Component</h1>;
+  const [values, setValues] = useState(InitialState);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
