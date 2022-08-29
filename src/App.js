15cf1158d072c017b1901cbde19ae9599e06a4f1
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-08-29 16:43:04 +0300

    add stayled css in component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import Landing from "./pages/Landing";
 import "./App.css";
-import styled from "styled-components";
-
-const Button = styled.button`
-  background: red;
-  color: white;
-  font-size: 2rem;
-`;
 
 function App() {
   return (
     <div>
-      <Button>Click Me</Button>
       <Landing />
     </div>
   );
