9d99de51af534c4f72dd24b2c3c1df7b07f0d657
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-08-29 19:25:04 +0300

    react router initial setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Landing, Error, Dashboard, Register } from "./pages/index";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Dashboard />
-      {/* <Error /> */}
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,6 +3,7 @@ import React from "react";
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components/index";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -22,7 +23,9 @@ const Landing = () => {
             animi repudiandae eum architecto praesentium soluta nemo ex earum
             laborum reprehenderit corporis inventore!
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
