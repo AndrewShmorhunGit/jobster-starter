a53b8090f3f6f18e72382b44db9281f9596b6ba3
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-08-29 15:52:53 +0300

    styled component example

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,7 +3,29 @@ import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 
 const Landing = () => {
-  return <h1>Landing page</h1>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobstar logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* {info} */}
+        <div className="info">
+          <h1>
+            job <span> tracking</span> app
+          </h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+            consequuntur, ipsam, ipsa numquam dignissimos aliquid in debitis
+            animi repudiandae eum architecto praesentium soluta nemo ex earum
+            laborum reprehenderit corporis inventore!
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
