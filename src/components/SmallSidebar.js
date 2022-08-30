6bdc6550d1fe5f86d4df0c33c5f491af6d10fe18
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-08-31 02:14:57 +0300

    adapt smallSidebar

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,9 +1,28 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import { FaTimes } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { store } from "../store";
+
 const SmallSidebar = () => {
+  const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
   return (
     <Wrapper>
-      <h1>SmallSidebar</h1>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button className="close-btn" onClick={() => console.log("toggle")}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">Some text</div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
