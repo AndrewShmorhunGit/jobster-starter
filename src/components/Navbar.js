81220decd3ba96f53a3a979f2bcd43976e619b9c
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-08-31 01:24:00 +0300

    setup Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,53 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/Navbar";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import Logo from "./Logo";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 const Navbar = () => {
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const toggleSidebar = () => {
+    console.log(`toggle sidebar`);
+  };
+
   return (
     <Wrapper>
-      <h1>Navbar</h1>
+      <div className="nav-center">
+        <button
+          type="button"
+          className="toggle-btn"
+          onClick={() => toggleSidebar()}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log("toggle login")}
+          >
+            <FaUserCircle />
+            {user?.name}
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => console.log("logout user")}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
