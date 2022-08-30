2a506a2fb1de7eeffc2c00f1899cb85d5b3aab42
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-08-31 00:20:23 +0300

    add dashboard folder set up App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import { Landing, Error, Dashboard, Register } from "./pages/index";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import {
+  AllJobs,
+  AddJob,
+  Profile,
+  SharedLayout,
+  Stats,
+} from "./pages/dashboard/index";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Stats />} />
+          <Route path="/all-jobs" element={<AllJobs />} />
+          <Route path="/add-job" element={<AddJob />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
         <Route path="/landing" element={<Landing />} />
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<Error />} />
