7cd52ce2b64ddb13a0f6dd78fc587bc84a9371ee
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-08-30 23:21:07 +0300

    propper register user

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -17,12 +17,10 @@ export const registerUser = createAsyncThunk(
   "user/loginUser",
   async (user, thunkAPI) => {
     try {
-      const resp = await customFetch.post("/auth/testingRegister", user);
-      console.log(thunkAPI);
-      console.log(resp);
+      const resp = await customFetch.post("/auth/register", user);
+      return resp.data;
     } catch (error) {
-      toast.error(error.response.data.msg);
-      console.log(error.response);
+      return thunkAPI.rejectWithValue.error.response.data.msg;
     }
   }
 );
@@ -30,6 +28,21 @@ export const registerUser = createAsyncThunk(
 const userSlice = createSlice({
   name: "user",
   initialState,
+  extraReducers: {
+    [registerUser.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [registerUser.fulfilled]: (state, { payload }) => {
+      const { user } = payload;
+      state.isLoading = false;
+      state.user = user;
+      toast.success(`Hello There ${user.name}`);
+    },
+    [registerUser.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
+  },
 });
 
 export default userSlice.reducer;
