033a80e1dd13633cffb6529c8bd6025178168424
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-09-01 23:55:24 +0300

    add Profile variables

--- src/pages/dashboard/Profile.js
+++ src/pages/dashboard/Profile.js
@@ -5,7 +5,32 @@ import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 
 const Profile = () => {
-  return <h1>Profile</h1>;
+  const { isLoading, user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const [userData, setUserData] = {
+    name: user?.name || "",
+    email: user?.email || "",
+    lastName: user?.lastName || "",
+    location: user?.location || "",
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, lastName, location } = userData;
+
+    if (!name || !email || !lastName || !location) {
+      toast.error("Please Fill Out All Fields");
+      return;
+    }
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserData({ ...userData, [name]: value });
+  };
+  return <Wrapper>Profile</Wrapper>;
 };
 
 export default Profile;
