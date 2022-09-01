30d72bcbd7ab443c55645dc7d140f8ff0e6189ba
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-09-02 00:17:55 +0300

    setup Profile component

--- src/pages/dashboard/Profile.js
+++ src/pages/dashboard/Profile.js
@@ -8,12 +8,12 @@ const Profile = () => {
   const { isLoading, user } = useSelector((store) => store.user);
   const dispatch = useDispatch();
 
-  const [userData, setUserData] = {
+  const [userData, setUserData] = useState({
     name: user?.name || "",
     email: user?.email || "",
     lastName: user?.lastName || "",
     location: user?.location || "",
-  };
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -30,7 +30,43 @@ const Profile = () => {
     const value = e.target.value;
     setUserData({ ...userData, [name]: value });
   };
-  return <Wrapper>Profile</Wrapper>;
+  return (
+    <Wrapper>
+      <form className="form" onSubmit={handleSubmit}>
+        <h3>profile</h3>
+        <div className="form-center">
+          <FormRow
+            type="text"
+            name="name"
+            value={userData.name}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="text"
+            labelText="last name"
+            name="lastName"
+            value={userData.lastName}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="email"
+            name="email"
+            value={userData.email}
+            handleChange={handleChange}
+          />
+          <FormRow
+            type="text"
+            name="location"
+            value={userData.location}
+            handleChange={handleChange}
+          />
+          <button type="submit" className="btn btn-block" disabled={isLoading}>
+            {isLoading ? "Please wait..." : "save changes"}
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Profile;
