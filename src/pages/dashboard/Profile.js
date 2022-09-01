db6b1a00640f95a42c82c6d6fb6774796b8e9264
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-09-01 23:47:49 +0300

    Profile import

--- src/pages/dashboard/Profile.js
+++ src/pages/dashboard/Profile.js
@@ -1,4 +1,8 @@
-import React from "react";
+import { useState } from "react";
+import { FormRow } from "../../components";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 const Profile = () => {
   return <h1>Profile</h1>;
