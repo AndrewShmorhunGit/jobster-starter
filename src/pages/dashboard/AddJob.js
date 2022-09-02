f8b4398b421e2179ca83b45e39dcfa3ab1efb48f
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-09-02 23:34:28 +0300

    save jobLocation, addJob final

--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -7,6 +7,7 @@ import {
   clearValues,
   createJob,
 } from "../../features/job/jobSlice";
+import { useEffect } from "react";
 
 const AddJob = () => {
   const {
@@ -22,6 +23,7 @@ const AddJob = () => {
     editJobId,
   } = useSelector((store) => store.job);
 
+  const { user } = useSelector((store) => store.user);
   const dispatch = useDispatch();
 
   const handlerSubmit = (e) => {
@@ -40,6 +42,10 @@ const AddJob = () => {
     const value = e.target.value;
     dispatch(handleChange({ name, value }));
   };
+
+  useEffect(() => {
+    dispatch(handleChange({ name: "jobLocation", value: user.location }));
+  }, []);
 
   return (
     <Wrapper>
