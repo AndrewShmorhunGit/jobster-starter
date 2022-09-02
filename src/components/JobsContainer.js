bfd0f2833249061b774a9b42c64d419d19f41fb1
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-09-03 01:12:18 +0300

    get allJobs request

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -3,11 +3,16 @@ import Job from "./Job";
 import Wrapper from "../assets/wrappers/JobsContainer";
 import { useSelector, useDispatch } from "react-redux";
 import Loading from "./Loading";
+import { getAllJobs } from "../features/allJobs/allJobsSlice";
 
 const JobsContainer = () => {
   const { jobs, isLoading } = useSelector((store) => store.allJobs);
 
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllJobs());
+  }, []);
 
   if (isLoading) {
     return <Loading center />;
@@ -23,7 +28,7 @@ const JobsContainer = () => {
 
   return (
     <Wrapper>
-      <h5>jobs ifo</h5>
+      <h5>jobs info</h5>
       <div className="jobs">
         {jobs.map((job) => {
           return <Job key={job._id} {...job} />;
