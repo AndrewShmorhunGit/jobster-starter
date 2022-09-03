d79aab174ee8036ed07b450fae1753042e752398
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-09-04 00:33:43 +0300

    add authHeader 3d way, axios approach

--- src/features/job/JobThunk.js
+++ src/features/job/JobThunk.js
@@ -6,7 +6,7 @@ import authHeader from "../../utils/authHeaders";
 
 export const createJobThunk = async (job, thunkAPI) => {
   try {
-    const resp = await customFetch.post("/jobs", job, authHeader(thunkAPI));
+    const resp = await customFetch.post("/jobs", job);
     thunkAPI.dispatch(clearValues());
     return resp.data;
   } catch (error) {
@@ -21,10 +21,7 @@ export const createJobThunk = async (job, thunkAPI) => {
 export const deleteJobThunk = async (jobId, thunkAPI) => {
   thunkAPI.dispatch(showLoading());
   try {
-    const resp = await customFetch.delete(
-      `/jobs/${jobId}`,
-      authHeader(thunkAPI)
-    );
+    const resp = await customFetch.delete(`/jobs/${jobId}`);
     thunkAPI.dispatch(getAllJobs());
     return resp.data.msg;
   } catch (error) {
@@ -36,11 +33,7 @@ export const deleteJobThunk = async (jobId, thunkAPI) => {
 
 export const editJobThunk = async ({ jobId, job }, thunkAPI) => {
   try {
-    const resp = await customFetch.patch(
-      `/jobs/${jobId}`,
-      job,
-      authHeader(thunkAPI)
-    );
+    const resp = await customFetch.patch(`/jobs/${jobId}`, job);
     thunkAPI.dispatch(clearValues());
     return resp.data;
   } catch (error) {
