385750e7c5968858bf7888510b10d0ebf36bfb95
Andrew Shmorhun <andrewshmorgun@gmail.com>
2022-09-05 21:39:46 +0300

    query params complete setup

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -7,9 +7,22 @@ import { getAllJobs } from "../features/allJobs/allJobsSlice";
 import PageBtnContainer from "./PageBtnContainer";
 
 const JobsContainer = () => {
-  const { jobs, isLoading, page, totalJobs, numOfPages } = useSelector(
-    (store) => store.allJobs
-  );
+  const {
+    jobs,
+    isLoading,
+    page,
+    totalJobs,
+    numOfPages,
+    search,
+    searchStatus,
+    searchType,
+    sort,
+  } = useSelector((store) => store.allJobs);
+
+  useEffect(() => {
+    dispatch(getAllJobs());
+    // eslint-disable-next-line
+  }, [page, search, searchStatus, searchType, sort]);
 
   const dispatch = useDispatch();
 
